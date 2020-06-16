b59f0f2575714cee36008eebfb7d55ec3807ce74
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-06-16 17:55:54 -0400

    have added code to catch err and to catch a 404 error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if(!res.ok){
+    throw new Error(`Could not fetch ${url}` + `, recieved ${res.status}`);
+  } 
   const body = await res.json();
   return body;
 };
 
-getResource('https://swapi.dev/api/people/1/')
+getResource('https://swapi.dev/api/people/1222235/')
   .then((body) => {
     console.log(body);
-  });
+  })
+    .catch((err) => {
+      console.log(err);
+    });
