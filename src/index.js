a81d997fd02080cf94205d9e16093fdbdda6ecf6
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-06-15 20:01:50 -0400

    adding fetch and async await function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+fetch('https://swapi.dev/api/people/1/')
+  .then((res) => {
+    return res.json();
+  })
+  .then((body) => {
+    console.log('Now lady and gentelman', body);
+  });
