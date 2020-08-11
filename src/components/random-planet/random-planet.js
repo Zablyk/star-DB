b1affe981f35f06d6b9be3e9dc924e0f1d003b9a
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-08-11 19:12:24 -0400

    return a code back not using spinner and now will emplement a logic for spinner

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -36,7 +36,25 @@ export default class RandomPlanet extends Component {
 
     return (
       <div className="random-planet jumbotron rounded">
-        <Spinner />
+        <img className="planet-image" alt="Random Planet"
+             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+        <div>
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Population</span>
+              <span>{population}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Rotation Period</span>
+              <span>{rotationPeriod}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Diameter</span>
+              <span>{diameter}</span>
+            </li>
+          </ul>
+        </div>
       </div>
 
     );
