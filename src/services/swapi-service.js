00b683b7ce37e7d2a8e5efa2d1669d08959474c4
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-08-11 17:44:05 -0400

    added a _transornmPlanet code which contains regular expression which takin id of planet from url

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -18,10 +18,11 @@ export default class SwapiServices {
     };
     async getAllPlanets() {
       const res = await this.getResource(`/planets/`);
-      return res.results;
+      return res.results.map(this._transformPlanet);
     };
-    getPlanet(id) {
-      return this.getResource(`/planets/${id}/`);
+    async getPlanet(id) {
+      const planet = await this.getResource(`/planets/${id}/`);
+      return this._transformPlanet(planet);
     };
     async getAllStarships () {
       const res = await this.getResource(`/starships/`);
@@ -30,11 +31,21 @@ export default class SwapiServices {
     getStarship (id) {
       return this.getResource(`/starships/${id}/`);
     };
+
+    _transformPlanet(planet) {
+      const idRegExp = /\/([0-9]*)\/$/;
+      const id = planet.url.match(idRegExp)[1]; 
+
+      return {
+        id,
+        name: planet.name,
+        population: planet.population,
+        rotationPeriod: planet.rotation_period,
+        diameter: planet.diameter
+      }
+    };
+
   };
   
-  const swapi = new SwapiServices();
-  
-  swapi.getPerson(3).then((p) => {
-      console.log(p.name);
-    });
+
   
