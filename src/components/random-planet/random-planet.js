7a1f178fed0edb5372e1348065436c63a64e1ad8
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-08-13 17:24:59 -0400

    have added error-indicator component and implement it in to random-planet by adding logic

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -4,6 +4,7 @@ import SwapiService from '../../services/swapi-service';
 
 import './random-planet.css';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
 export default class RandomPlanet extends Component {
 
@@ -11,13 +12,14 @@ export default class RandomPlanet extends Component {
 
   state = {
     planet: {},
-    loading: true
+    loading: true,
+    error: false
   };
 
   constructor() {
     super();
     this.updatePlanet();
-  }
+  };
 
   onPlanetLoaded = (planet) => {
     this.setState({
@@ -25,24 +27,37 @@ export default class RandomPlanet extends Component {
       loading: false });
   };
 
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false
+    });
+  };
+
   updatePlanet() {
-    const id = Math.floor(Math.random()*25) + 2;
+    const id = Math.floor(Math.random()*25) + 200000;
     this.swapiService
       .getPlanet(id)
-      .then(this.onPlanetLoaded);
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
 
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
+    
+    const hasData = !(loading || error);
+    const errorMessage = error ? <ErrorIndicator/> : null; 
+    
 
-    if(loading) {
-      return <Spinner />
-    }  
+    const spinner = loading ? <Spinner /> : null;
+    const content = hasData ? <PlanetView planet = {planet}/> : null
 
     return (
       <div className="random-planet jumbotron rounded">
-        <PlanetView planet = {planet}/>
+        { errorMessage }
+        { spinner }
+        { content }
       </div>
 
     );
