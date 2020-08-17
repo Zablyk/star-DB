ca0ccd16a00c308aea61cf78f11925859e0bdaea
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-08-17 17:39:24 -0400

    added setInterval to call function to show random planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -19,6 +19,7 @@ export default class RandomPlanet extends Component {
   constructor() {
     super();
     this.updatePlanet();
+    setInterval(this.updatePlanet, 2500);
   };
 
   onPlanetLoaded = (planet) => {
@@ -34,8 +35,8 @@ export default class RandomPlanet extends Component {
     });
   };
 
-  updatePlanet() {
-    const id = Math.floor(Math.random()*25) + 2;
+  updatePlanet = () => {
+    const id = Math.floor(Math.random()*25) + 3;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
