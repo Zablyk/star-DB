e1b09d48810f0850090a9b2ea9ef1490357e2d0f
Oleg Zablotskyi <Zablotsky@gmail.com>
2020-09-10 19:47:46 -0400

    added componentDidMout and Spinner in to item-list and fixed mistake in swapi-service

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 import './item-list.css';
+import SwapiServices from '../../services/swapi-service';
+import Spinner from '../spinner';
 
 export default class ItemList extends Component {
 
+  swapiService = new SwapiServices();
+
+  state = {
+    peopleList: null
+  };
+
+  componentDidMount() {
+    this.swapiService
+      .getAllPeople()
+      .then((peopleList) => {
+        this.setState({
+          peopleList
+        });
+      });
+  };
+
   render() {
+
+    const { peopleList } = this.state;
+
+    if(!peopleList) {
+      return <Spinner/>;
+    }
+
     return (
       <ul className="item-list list-group">
         <li className="list-group-item">
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -39,7 +39,7 @@ export default class SwapiServices {
       return item.url.match(idRegExp)[1]; 
     }
 
-    _transformPlanet(planet) {
+    _transformPlanet = (planet) => {
        return {
         id: this._extractId(planet),
         name: planet.name,
@@ -49,7 +49,7 @@ export default class SwapiServices {
       };
     };
 
-    _transformStarship(starship) {
+    _transformStarship = (starship) => {
       return {
           id: this._extractId(starship),
           name: starship.name,
@@ -63,7 +63,7 @@ export default class SwapiServices {
       };
     }; 
 
-    _transformPerson(person) {
+    _transformPerson = (person) => {
       return {
         id: this._extractId(person),
         name: person.name,
